3bc2ba1765da5b8f84bc6469799ae3fe6dd826da
sahilnayak <snayak.researcher@gmail.com>
2016-12-10 14:43:49 -0800

    Created a basic redux store from scratch and also added a render method to update the application when the state is changed

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+*/
 
-// Creating a Basic Increment action
-
+// Creating a Basic Increment Reducer that updates the state
 const counter = (state = 0, action) => {
     switch(action.type) {
         case 'INCREMENT':
@@ -16,5 +17,43 @@ const counter = (state = 0, action) => {
     }
 }
 
+// Creates a store using the reducer to tell the store how to update.
+// Store has 3 methods: getState, dispatch(dispatches action) and subscribe(updates the UI with the new state)
+const createStore = (reducer) => {
+    let state;
+    let listeners = [];
 
-const store = createStore(counter)
+    const getState = () => state;
+
+    const dispatch = (action) => {
+        state = reducer(state, action);
+        listeners.forEach(listener => listener());
+    }
+
+    const subscribe = (listener) => {
+        listeners.push(listener);
+        return () => {
+            listeners = listeners.filter(l => l !== listener);
+        }
+    }
+
+    dispatch({});
+
+    return {getState, dispatch, subscribe };
+}
+
+//Create the inital store
+const store = createStore(counter);
+
+//Creates a render method that is called when the app is started
+const render = () => {
+    ReactDOM.render(
+        <App />,
+        document.getElementById('root');
+    );
+}
+
+
+//Adds render as a listener so whenever changes are made to the state the application renders again
+store.subscribe(render);
+render();
