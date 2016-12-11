7c854b299cf02213610f3d6df87343ebaa436137
sahilnayak <snayak.researcher@gmail.com>
2016-12-10 18:17:12 -0800

    Created combine reducer that has both the todos and visbilityfilter reducers

--- index.js
+++ index.js
@@ -6,16 +6,56 @@ import { createStore, applyMiddleware } from 'redux';
 */
 
 // Creating a Basic Increment Reducer that updates the state
-const counter = (state = 0, action) => {
+
+// Created a reducer helper 
+const todo = (state, action) => {
     switch(action.type) {
-        case 'INCREMENT':
-            return state + 1;
-        case 'DECREMENT':
-            return state - 1;
+        case 'ADD_TODO':
+            return {
+                id: action.id,
+                text: action.text,
+                completed: false
+            }
+        case 'TOGGLE_TODO':
+            if(todo.id !== action.id) return todo;
+            return {
+                ...todo,
+                completed: !todo.completed
+            };
         default:
             return state;
     }
 }
+
+//Reducer #1 to manage the ToDo list
+const todos = (state = [], action) => {
+    switch(action.type) {
+        case 'ADD_TODO':
+            return [...state , todo(undefined, action)];
+        case 'TOGGLE_TODO':
+            return state.map(t => todo(t, action));
+        default:
+            return state;
+    }
+}
+
+//Reducer #2 to manage the visibiltyFilter
+const visibiltyFilter = (state = 'SHOW_ALL' , action) => {
+    switch(action.type) {
+        case 'SET_VISIBILITY_FILTER':
+            return action.filter;
+        default:
+            return state;
+    }
+}
+
+//Combine Reducer, combines the Todo and Visibility reducer into one reducer
+
+const combineReducer = (state = {}, action) => {
+    
+}
+
+
 
 // Creates a store using the reducer to tell the store how to update.
 // Store has 3 methods: getState, dispatch(dispatches action) and subscribe(updates the UI with the new state)
